0c5cfcc77ca686839fd6d72df8e5fed9f787968c
flurinasophie <flurina.baumbach@gmail.com>
2026-03-23 14:40:05 +0100

    Changed the icons of the different Leistungen

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // Öffnet automatisch den Browser, wenn du npm run dev startest
+    port: 5173  // Legt den Standard-Port fest
+  }
 })
